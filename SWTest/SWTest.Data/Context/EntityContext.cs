b39b684addb3af345738413761428e159693e55a
Galu <galluppi.leandro@hotmail.com>
2016-11-11 21:07:59 -0300

    Set up project.

---
+++ SWTest/SWTest.Data/Context/EntityContext.cs
@@ -0,0 +1,21 @@
+﻿using SWTest.Data.Entities;
+using System.Data.Entity;
+
+namespace SWTest.Data.Context
+{
+    public class EntityContext : DbContext
+    {
+        public EntityContext()
+            : base("DBConnection")
+        {
+            DbConfiguration.SetConfiguration(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ SWTest/SWTest.Data/Entities/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWTest.Data.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Tecnology { get; set; }
+
+        public DateTime StartingDate { get; set; }
+
+        public string RegistrationLink { get; set; }
+    }
+}
